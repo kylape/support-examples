10643b84e92717ddf11011111321bb42ecd67430
Kyle Lape <kyle.lape@redhat.com>
2013-01-16 12:47:40 -0600

    Adding CXF spring example

---
+++ helloWorldCxfSpring.war/README.md
@@ -0,0 +1,3 @@
+###Simple JBossWS-CXF web service with Spring
+
+This shows how to set up a simple `jbossws-cxf.xml` file to be picked up by JBossWS to configure CXF via a Spring beans file.  Spring will need to be available to your deployment's classpath, whether that be in a JBoss Module or in your deployment.
---
+++ helloWorldCxfSpring.war/WEB-INF/classes/com/redhat/gss/jaxws/HelloWS.java
@@ -0,0 +1,27 @@
+/*
+ * To the extent possible under law, Red Hat, Inc. has dedicated all copyright 
+ * to this software to the public domain worldwide, pursuant to the CC0 Public 
+ * Domain Dedication. This software is distributed without any warranty.  See 
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+package com.redhat.gss.jaxws;
+
+import javax.xml.ws.spi.Provider;
+import org.jboss.logging.Logger;
+
+@javax.jws.WebService
+//@org.jboss.wsf.spi.annotation.WebContext(transportGuarantee="CONFIDENTIAL")
+//@org.apache.cxf.annotations.Logging(pretty=true)
+//@org.apache.cxf.feature.Features(features={"org.apache.cxf.feature.LoggingFeature"})
+public class HelloWS
+{
+  private Logger log = Logger.getLogger(this.getClass().getName());
+
+  public String hello(String name) throws Exception
+  {
+    Provider provider = Provider.provider();
+    log.info("WS Provider: " + provider.getClass().getName());
+    return "Hello, " + name;
+  }
+}
