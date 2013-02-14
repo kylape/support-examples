ee9261445b53b9924d59470fac364d1371a3b9ac
Kyle Lape <kyle.lape@redhat.com>
2013-02-14 11:50:13 -0600

    Changing jar to rar to better match use case

--- logging/deploymentDependencyIssue/runTest.sh
+++ logging/deploymentDependencyIssue/runTest.sh
@@ -12,7 +12,7 @@ fi
 
 echo "Cleaning logs..."
 rm *.log
-cd deploymentB.jar
+cd deploymentB.rar
 ant deploy
 cd ../deploymentA.war
 ant deploy
@@ -34,6 +34,5 @@ tail my-log4j.log
 echo "================================================"
 echo
 JBOSS_PID=`jps | grep jboss-modules | cut -f 1 -d " "`
-echo "Killing JBoss..."
 echo "kill $JBOSS_PID"
 kill $JBOSS_PID
