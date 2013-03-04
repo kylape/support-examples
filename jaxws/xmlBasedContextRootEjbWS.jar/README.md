5d8f3471fbbeeaf3ab8092c16bac540e3e86f3b4
Kyle Lape <kyle.lape@redhat.com>
2013-03-04 10:02:55 -0600

    Adding xml-based context root config example

---
+++ jaxws/xmlBasedContextRootEjbWS.jar/README.md
@@ -0,0 +1,3 @@
+###How to set context root via XML
+
+Uses `jboss-webservices.xml` to set the context root.
---
+++ jaxws/xmlBasedContextRootEjbWS.jar/com/redhat/gss/jaxws/TestClient.java
@@ -0,0 +1,45 @@
+package com.redhat.gss.jaxws;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.net.URL;
+
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.Service;
+import javax.xml.namespace.QName;
+
+import org.jboss.ws.core.StubExt;
+
+public class TestClient
+{
+  public static void main(String[] args) throws Exception
+  {
+    //JBossWS-CXF
+    //URL wsdl = new URL("http://localhost:8080/helloWorldEjb/HelloWS/HelloWSImpl?wsdl");
+    
+    //JBossWS-Native
+    URL wsdl = new URL("http://localhost:8080/helloWorldEjb/HelloWSImpl?wsdl");
+
+    QName qname = new QName("http://jaxws.gss.redhat.com/", "HelloWS");
+    final Service service = Service.create(wsdl, qname);
+
+    Runnable r = new Runnable() {
+      public void run()
+      {
+        HelloWS port = service.getPort(HelloWS.class);
+        for(int i=0; i<10; i++)
+        {
+          System.out.println(Thread.currentThread().getName() + ": " + port.hello("Kyle"));
+        }
+      }
+    };
+
+    for(int i=0; i<4; i++)
+    {
+      new Thread(r).start();
+    }
+  }
+}
