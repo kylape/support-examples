0a122f809787da15a3092ebaa8594733f5aa1460
Kyle Lape <kyle.lape@redhat.com>
2013-05-28 15:31:38 -0700

    Removing exception test case code

--- jaxws/webServiceRefTest.war/WEB-INF/classes/com/redhat/gss/jaxws/ClientEndpointImpl.java
+++ jaxws/webServiceRefTest.war/WEB-INF/classes/com/redhat/gss/jaxws/ClientEndpointImpl.java
@@ -12,21 +12,13 @@ public class ClientEndpointImpl
   @WebServiceRef(type=HelloWSService.class, wsdlLocation="META-INF/hello.wsdl")
   HelloWS port;
 
-  public String invokeHelloWorld(String name)
+  public String invokeHelloWorld(String name) throws Exception
   {
-    try
-    {
-      // QName serviceName = new QName("http://jaxws.gss.redhat.com/", "HelloWS");
-      // URL wsdl = new URL("http://localhost:8080/helloWorld/hello?wsdl");
-      // Service service = Service.create(wsdl, serviceName);
-      // HelloWS port = service.getPort(HelloWS.class);
-      return port.hello("Kyle");
-    }
-    catch(Exception e)
-    {
-      e.printStackTrace();
-      return "Fail";
-    }
+    // QName serviceName = new QName("http://jaxws.gss.redhat.com/", "HelloWS");
+    // URL wsdl = new URL("http://localhost:8080/helloWorld/hello?wsdl");
+    // Service service = Service.create(wsdl, serviceName);
+    // HelloWS port = service.getPort(HelloWS.class);
+    return port.hello("Kyle");
   }
 }
 
