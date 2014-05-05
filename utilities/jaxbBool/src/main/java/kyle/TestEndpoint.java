94bf74aec87c78076c706b9ef29c06c496251962
Kyle Lape <kyle.lape@redhat.com>
2014-05-05 15:04:49 -0700

    Solved JAXB UnmarshalException issue

--- utilities/jaxbBool/src/main/java/kyle/TestEndpoint.java
+++ utilities/jaxbBool/src/main/java/kyle/TestEndpoint.java
@@ -1,6 +1,12 @@
 package kyle;
 
+import org.apache.cxf.annotations.EndpointProperties;
+import org.apache.cxf.annotations.EndpointProperty;
+
 @javax.jws.WebService
+@EndpointProperties({
+  @EndpointProperty(key="set-jaxb-validation-event-handler",value="false")
+})
 public class TestEndpoint
 {
   public void test(BooleanContainer bools)
